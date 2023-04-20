8b1d7964a1888a73f354253a3cdb00b9767b4c99
shogo-shimizu <shimizu.martin@gmail.com>
2023-04-20 21:44:57 +0900

    first commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer>
+    <div class="footerTitle">
+        <h2><?php bloginfo('name'); ?></h2>
+        <div class="snsFooter">
+            <a href="https://twitter.com/fox_hare_/">
+                <i class="fa-brands fa-twitter fa-size"></i>
+            </a>
+            <a href="https://www.instagram.com/fox_hare_/">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+            <!-- <i class="fa-brands fa-youtube"></i> -->
+        </div>
+    </div>
+    <div class="footerInfo">
+        <h2>Blog Infomation</h2>
+        <p>お問合せ</p>
+        <p><?php bloginfo('admin_email') ?></p>
+        <p></p>
+    </div>
+    <!-- <div class="footerContact">
+        <h2>Contact</h2>
+        <p>お問合せ</p>
+    </div> -->
+</footer>
+<?php wp_footer(); ?>
